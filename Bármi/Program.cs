93128782b217aa9fab9cd6d135051a067272a2a8
leveses <boda.zsombor10@gmail.com>
2020-10-13 11:18:34 +0200

    sql lekerdezés C#-ban

--- Bármi/Program.cs
+++ Bármi/Program.cs
@@ -24,10 +24,17 @@ namespace Bármi
 
             var iBetusok = from nev in nevek where nev.Contains('i') select nev;
 
+            int[] szamok = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            var szamokNagyobb = from szam in szamok where szam > 5 select szam;
             Console.WriteLine("A nevek amelyek tartalmazzák az i betűt:");
             foreach (var i in iBetusok)
             {
                 Console.WriteLine(i);
+            }
+            Console.Write("Számok amelyek nagyobbak 5-nél: ");
+            foreach (var i in szamokNagyobb)
+            {
+                Console.Write(i+ " ");
             }
 
             Console.ReadKey();
